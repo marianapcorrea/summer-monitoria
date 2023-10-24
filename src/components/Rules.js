901d717f7f38e981ccdad44f1fc33bbab53c2d45
Mariana Correa <mpcs270391@gmail.com>
2023-10-24 20:32:07 -0300

    Adiciona regra

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
-const Rules = () => {
+function Rules() {
   return (
-<>
-<h2>COMO JOGAR TRYUNFO</h2>
-
-</>  )
+    <>
+      <h2>COMO JOGAR TRYUNFO</h2>
+      <ol>
+        <li>As cartas são embaralhadas e distribuídas. Cada jogador forma um monte em suas mãos, de tal modo que possa ver apenas a carta de cima. Começa o jogo quem estiver à esquerda do jogador que distribuiu as cartas.</li>
+      </ol>
+  </>);
 }
 
-export default Rules
+export default Rules;
